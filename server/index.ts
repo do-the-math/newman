abcdcfedc9a9335fca528ede3e2d99b7c7890cab
Aman Nidhi <aman.sharefiles@gmail.com>
2019-12-21 00:09:57 +0530

    typo

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import { bootstrapMicroframework } from 'microframework';
 import { expressLoader } from './loaders/express.loader';
 import { corsLoader } from './loaders/cors.loader';
-import { helmetLoader } from './loaders/helmet.loader';
 import { initLoader } from './loaders/init.loader';
 import { logError, logInfo } from './utils/log';
 
