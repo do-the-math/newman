458bcc7f4a72371bcea8f57f61d1f23dcdbbb3aa
Aman Nidhi <aman.sharefiles@gmail.com>
2019-12-25 02:41:48 +0530

    added router controller decorator pattern structure for apis

--- server/api/controllers/v1/auth/auth.controller.ts
+++ server/api/controllers/v1/auth/auth.controller.ts
@@ -1,7 +1,27 @@
-import { Request, Response } from '../../../../types/express.extensions';
+import 'reflect-metadata';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  OnUndefined,
+  Post,
+  Req,
+  Res,
+} from 'routing-controllers';
+import { Request, Response } from './../../../../types/express.extensions';
 
-export default class AuthController {
-  public login = (req: Request, res: Response): void => {
-    res.send('authentication');
-  };
+@Controller(`/v1/auth`)
+export class AuthController {
+  @HttpCode(200)
+  @Get('/')
+  @OnUndefined(404)
+  getAll(@Req() request: Request, @Res() response: Response): any {
+    return undefined;
+  }
+
+  @Post('/')
+  @HttpCode(201)
+  getOne(): string {
+    return 'This action returns user #';
+  }
 }
